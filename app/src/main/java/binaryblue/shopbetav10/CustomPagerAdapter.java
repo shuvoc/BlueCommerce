8245a4ad534959a084774b7d475a37b75214dae6
Jobayed Ullah Shuvo <shuvo.sfl@gmail.com>
2016-08-08 11:02:04 +0600

    Replace with V1
    v1

---
+++ app/src/main/java/binaryblue/shopbetav10/CustomPagerAdapter.java
@@ -0,0 +1,76 @@
+package binaryblue.shopbetav10;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jobayed Ullah on 8/8/2016.
+ */
+
+enum CustomPagerEnum
+{
+
+    RED(R.string.red, R.layout.frag1),
+    BLUE(R.string.blue, R.layout.frag1),
+    ORANGE(R.string.orange, R.layout.frag1);
+
+    private int mTitleResId;
+    private int mLayoutResId;
+
+    CustomPagerEnum(int titleResId, int layoutResId) {
+        mTitleResId = titleResId;
+        mLayoutResId = layoutResId;
+    }
+
+    public int getTitleResId() {
+        return mTitleResId;
+    }
+
+    public int getLayoutResId() {
+        return mLayoutResId;
+    }
+
+}
+public class CustomPagerAdapter extends PagerAdapter {
+
+    private Context mContext;
+
+    public CustomPagerAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup collection, int position)
+    {
+        CustomPagerEnum customPagerEnum = CustomPagerEnum.values()[position];
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        ViewGroup layout = (ViewGroup) inflater.inflate(customPagerEnum.getLayoutResId(), collection, false);
+        collection.addView(layout);
+        return layout;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup collection, int position, Object view) {
+        collection.removeView((View) view);
+    }
+
+    @Override
+    public int getCount() {
+        return CustomPagerEnum.values().length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        CustomPagerEnum customPagerEnum = CustomPagerEnum.values()[position];
+        return mContext.getString(customPagerEnum.getTitleResId());
+    }
+
+}
